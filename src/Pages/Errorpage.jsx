7eaef68e370f817edaa123b6a02138c33e144b20
Onajobi James <jamesonajobi123@gmail.com>
2022-10-24 01:25:15 -0700

    Adding some content to the 404 page

--- src/Pages/Errorpage.jsx
+++ src/Pages/Errorpage.jsx
@@ -2,9 +2,10 @@ import React from "react"
 
 export default function Error(){
   return(
-    <div>
+    <div className="ErrorPage">
       <h1>404</h1>
-      <h3>Page Not Found</h3>
+      <h3>Not Found</h3>
+      <p>The resource requested could not be found</p>
     </div>
   )
 }
